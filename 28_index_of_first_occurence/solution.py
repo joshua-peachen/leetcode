2d8a5f9494d89e86860e31798c80b74b90ee8950
Joshua Peachen <pm3fm7@icloud.com>
2024-12-12 21:48:43 +0100

    start solving task n 28

---
+++ 28_index_of_first_occurence/solution.py
@@ -0,0 +1,21 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for ind in range(len(haystack) - len(needle)):
+            substr = haystack[ind:ind+len(needle)]
+            if substr == needle:
+                return ind
+        return -1
+
+if __name__ == '__main__':
+    sol = Solution()
+    test_cases = [
+        ("sadbutsad", "sad", 0),
+        ("leetcode", "leeto", -1),
+        ("a", "a", 0),
+        ("ab", "b", 1),
+        ("a", "b", -1),
+        ("ab", "ab", 0)
+    ]
+    for haystack, needle, expected in test_cases:
+        actual = sol.strStr(haystack, needle)
+        print(f'{haystack = }, {needle = }, {expected == actual}')
